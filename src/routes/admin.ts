61a9e54533caca290936ed5e5dc5922a2dd3bf3f
Maycon Kabitschke <mayconkabitschke@gmail.com>
2023-12-08 12:01:47 -0300

    CRUD Groups

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import * as auth from '../controllers/auth';
 import * as events from '../controllers/events';
+import * as groups from '../controllers/groups';
 
 const router = Router();
 
@@ -14,5 +15,12 @@ router.get('/events/:id', auth.validate, events.getEvent);
 router.post('/events', auth.validate, events.addEvent);
 router.put('/events/:id', auth.validate, events.updateEvent);
 router.delete('/events/:id', auth.login, events.deleteEvent);
+
+
+router.get('/events/:id_event/groups', auth.validate, groups.getAll);
+router.get('/events/:id_event/groups/:id', auth.validate, groups.getGroup);
+router.get('/events/:id_event/groups', auth.validate, groups.addGroup);
+router.put('/events/:id_event/groups/:id', auth.validate, groups.updateGrpup);
+router.delete('/events/:id_events/groups/:id', auth.validate, groups.deleteGroup);
 
 export default router;
