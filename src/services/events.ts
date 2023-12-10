73a3c15778b3b8b7ad237c24119399b21cc9904d
Maycon Kabitschke <mayconkabitschke@gmail.com>
2023-12-10 00:42:54 -0300

    sorteio 3

--- src/services/events.ts
+++ src/services/events.ts
@@ -58,9 +58,45 @@ export const doMatches = async (id: number): Promise<boolean> => {
 
 
                 let attempts = 0;
-                let maxAttempts = peopleList.lenght;
+                let maxAttempts = peopleList.length;
                 let keepTrying = true;
                 while(keepTrying && attempts < maxAttempts){
+
+                    keepTrying = false;
+                    attempts ++;
+                    sortedList = [];
+                    sortable = peopleList.map(item => item.id);
+
+                    for(let i in  peopleList){
+                        let sortableFiltered: number[] = sortable;
+                        if(eventItem.grouped){
+                            sortableFiltered = sortable.filter(sortableItem => {
+                                let sortablePerson = peopleList.find(item => item.id === sortableItem);
+                                return peopleList[i].id_group !== sortablePerson?.id_group;
+                            });
+
+                        }
+                        if(sortableFiltered.length === 0 || (sortableFiltered.length === 1 && peopleList[i].id === sortableFiltered[0])){
+                            keepTrying = true;
+                        }else{
+                            let sortedIndex = Math.floor(Math.random() * sortableFiltered.length);
+                            while(sortableFiltered[sortedIndex] === peopleList[i].id){
+                                sortedIndex = Math.floor(Math.random() * sortableFiltered.length);
+                            }
+
+                            sortedList.push({
+                                id: peopleList[i].id,
+                                match: sortableFiltered[sortedIndex]
+                            });
+
+                            sortable= sortable.filter(item => item !== sortableFiltered[sortedIndex]);
+                        }
+                        
+                    }
+
+                    
+
+ 
 
                 }
 
